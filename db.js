081014219c73cfbd2f94ddb0f7d2cc91a31de12e
Vincent Serem <seremvincent19@gmail.com>
2023-09-18 14:25:43 +0300

    Modified Database Connection

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const { default: mongoose } = require("mongoose");
 
 let db;
 
-mongoose.connect("mongodb://127.0.0.1:27017/ACMS")
+mongoose.connect("mongodb+srv://acms:iJ96LqwsH9xRWDB@cluster0.ho8xusg.mongodb.net/?retryWrites=true&w=majority ")
     .then(db => {
         db = db
         console.log("Database Connected")
